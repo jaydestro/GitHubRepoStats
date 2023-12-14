2fd9d1ec0805590cbb2d1dee4d29ad3025c034ff
Jay Gordon <2974195+jaydestro@users.noreply.github.com>
2023-12-14 11:27:53 -0500

    just storing this

--- report.py
+++ report.py
@@ -16,17 +16,17 @@ def get_github_data(api_url, token):
 
 # Function to process traffic data
 def process_traffic_data(data):
-    return [{"Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d').date(), 
+    return [{"Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d'), 
              "Views": item['count'], 
              "Unique visitors": item['uniques']} for item in data['views']]
 
 # Function to process clones data
 def process_clones_data(data):
-    return [{"Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d').date(), 
+    return [{"Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d'), 
              "Clones": item['count'], 
              "Unique cloners": item['uniques']} for item in data['clones']]
 
-# Updated function to process referrers data with timestamp
+# Function to process referrers data with timestamp
 def process_referrers_data(data):
     timestamp = datetime.now()
     return [{"Referring site": item['referrer'], 
@@ -34,7 +34,7 @@ def process_referrers_data(data):
              "Unique visitors": item['uniques'],
              "FetchedAt": timestamp} for item in data]
 
-# Updated function to process popular content data with timestamp
+# Function to process popular content data with timestamp
 def process_popular_content_data(data):
     timestamp = datetime.now()
     return [{"Path": item['path'], 
@@ -51,46 +51,77 @@ def read_token_from_file(file_path):
         print(f"Error reading token file: {e}")
         return None
 
+# Function to fetch historical data from MongoDB
+def fetch_historical_data(client, database_name, collection_name):
+    db = client[database_name]
+    collection = db[collection_name]
+    data = list(collection.find())
+    return pd.DataFrame(data)
+
 # Function to get the last recorded date from a DataFrame
-def get_last_recorded_date(df):
-    if df.empty or "Date" not in df.columns:
+def get_last_recorded_date(df, date_column='Date'):
+    if df.empty or date_column not in df.columns:
         return None
-    return pd.to_datetime(df["Date"]).max()
+    df[date_column] = pd.to_datetime(df[date_column], errors='coerce')
+    return df[date_column].max()
 
 # Function to append new data to the DataFrame
 def append_new_data(old_df, new_data, date_column):
     new_df = pd.DataFrame(new_data)
     if not old_df.empty:
-        last_date = get_last_recorded_date(old_df)
-        new_df = new_df[pd.to_datetime(new_df[date_column]) > last_date]
+        last_date = get_last_recorded_date(old_df, date_column)
+        if last_date is not None:
+            new_df = new_df[pd.to_datetime(new_df[date_column]) > last_date]
     return pd.concat([old_df, new_df], ignore_index=True)
-
-# Updated function to add grouped totals with optional referrer or content data handling
-def add_grouped_totals(df, date_column, metrics_columns, is_referrer_or_content=False):
-    if not df.empty:
-        if is_referrer_or_content:
-            df[date_column] = pd.to_datetime(df['FetchedAt']).dt.date
-        df[date_column] = pd.to_datetime(df[date_column])
-        monthly_totals = df.groupby(df[date_column].dt.to_period("M"))[metrics_columns].sum().reset_index()
-        monthly_totals[date_column] = monthly_totals[date_column].dt.strftime('Month %Y-%m')
-        yearly_totals = df.groupby(df[date_column].dt.to_period("Y"))[metrics_columns].sum().reset_index()
-        yearly_totals[date_column] = yearly_totals[date_column].dt.strftime('Year %Y')
-        return pd.concat([df, monthly_totals, yearly_totals], ignore_index=True)
-    return df
 
 # Function to create a MongoDB client
 def get_mongo_client(connection_string):
     return MongoClient(connection_string)
 
-# Function to upsert data to MongoDB
 def save_to_mongodb(client, database_name, collection_name, data):
     db = client[database_name]
     collection = db[collection_name]
-    unique_field = 'Date' if collection_name not in ['ReferringSites', 'PopularContent'] else 'Referring site' if collection_name == 'ReferringSites' else 'Path'
+
     for item in data:
-        query = {unique_field: item[unique_field]}
+        item.pop('_id', None)  # Remove the '_id' field if it exists
+        # Handle NaT values in the 'Date' field
+        if pd.isna(item.get('Date')):
+            continue  # Skip this record
+
+        query = {'Date': item['Date']}
         update = {"$set": item}
         collection.update_one(query, update, upsert=True)
+
+# Function to calculate daily, monthly, and yearly totals
+def calculate_daily_monthly_yearly_totals(df, date_column, count_column):
+    df[date_column] = pd.to_datetime(df[date_column], errors='coerce')
+    df = df.dropna(subset=[date_column])
+    df = df.sort_values(by=date_column).reset_index(drop=True)
+
+    # Calculate daily, monthly, and yearly totals
+    daily_totals = df.groupby(df[date_column].dt.to_period("D"))[count_column].sum()
+    monthly_totals = df.groupby(df[date_column].dt.to_period("M"))[count_column].sum()
+    yearly_totals = df.groupby(df[date_column].dt.to_period("Y"))[count_column].sum()
+
+    # Create a DataFrame to hold the summary rows
+    summary_df = pd.DataFrame({
+        date_column: [df[date_column].min()],  # Date of the first data point
+        count_column: [daily_totals.sum()],
+    })
+
+    # Append the summary rows for each month and year
+    for period, total in monthly_totals.items():
+        summary_df = pd.concat([summary_df, pd.DataFrame({
+            date_column: [period.to_timestamp()],  # Start of the month
+            count_column: [total],
+        })], ignore_index=True)
+    for period, total in yearly_totals.items():
+        summary_df = pd.concat([summary_df, pd.DataFrame({
+            date_column: [period.to_timestamp()],  # Start of the year
+            count_column: [total],
+        })], ignore_index=True)
+
+    return pd.concat([df, summary_df], ignore_index=True)
 
 # Main function with command-line interface
 def main():
@@ -99,7 +130,7 @@ def main():
     parser.add_argument('--owner', required=True, help='Organization/user name that owns the repository')
     parser.add_argument('--filename', help='Optional: Specify a filename for the Excel output. If not provided, defaults to {owner}-{repo}-traffic-data.xlsx')
     parser.add_argument('--token-file', required=True, help='Path to a text file containing the GitHub Personal Access Token')
-    parser.add_argument('--mongodb-connection-string', help='Optional: MongoDB connection string to store the data')
+    parser.add_argument('--mongodb-connection-string', required=True, help='MongoDB connection string to store and retrieve data')
     args = parser.parse_args()
 
     token = read_token_from_file(args.token_file)
@@ -109,25 +140,28 @@ def main():
     base_url = f"https://api.github.com/repos/{args.owner}/{args.repo}"
     filename = args.filename or f"{args.owner}-{args.repo}-traffic-data.xlsx"
 
-    # Fetch and process data
+    mongo_client = get_mongo_client(args.mongodb_connection_string)
+    database_name = "GitHubTrafficData"
+
+    historical_traffic_df = fetch_historical_data(mongo_client, database_name, "TrafficStats")
+    historical_clones_df = fetch_historical_data(mongo_client, database_name, "GitClones")
+    historical_referrers_df = fetch_historical_data(mongo_client, database_name, "ReferringSites")
+    historical_popular_content_df = fetch_historical_data(mongo_client, database_name, "PopularContent")
+
     views_data = get_github_data(f"{base_url}/traffic/views", token)
     clones_data = get_github_data(f"{base_url}/traffic/clones", token)
     referrers_data = get_github_data(f"{base_url}/traffic/popular/referrers", token)
     popular_content_data = get_github_data(f"{base_url}/traffic/popular/paths", token)
 
-    traffic_df = append_new_data(pd.DataFrame(), process_traffic_data(views_data), 'Date')
-    clones_df = append_new_data(pd.DataFrame(), process_clones_data(clones_data), 'Date')
-    referrers_df = append_new_data(pd.DataFrame(), process_referrers_data(referrers_data), 'Referring site')
-    popular_content_df = append_new_data(pd.DataFrame(), process_popular_content_data(popular_content_data), 'Path')
+    traffic_df = append_new_data(historical_traffic_df, process_traffic_data(views_data), 'Date')
+    clones_df = append_new_data(historical_clones_df, process_clones_data(clones_data), 'Date')
+    referrers_df = append_new_data(historical_referrers_df, process_referrers_data(referrers_data), 'FetchedAt')
+    popular_content_df = append_new_data(historical_popular_content_df, process_popular_content_data(popular_content_data), 'FetchedAt')
 
-    # Add grouped totals for Traffic Stats and Git Clones
-    traffic_df = add_grouped_totals(traffic_df, 'Date', ['Views', 'Unique visitors'])
-    clones_df = add_grouped_totals(clones_df, 'Date', ['Clones', 'Unique cloners'])
-    # Add grouped totals for Referring Sites and Popular Content with timestamp handling
-    referrers_df = add_grouped_totals(referrers_df, 'FetchedAt', ['Views', 'Unique visitors'], True)
-    popular_content_df = add_grouped_totals(popular_content_df, 'FetchedAt', ['Views', 'Unique visitors'], True)
+    # Calculate daily, monthly, and yearly totals for the 'traffic_df'
+    traffic_df = calculate_daily_monthly_yearly_totals(traffic_df, 'Date', 'Views')
 
-    # Save to Excel
+    # Save data to Excel and MongoDB
     with pd.ExcelWriter(filename, engine='openpyxl') as writer:
         traffic_df.to_excel(writer, sheet_name='Traffic Stats', index=False)
         clones_df.to_excel(writer, sheet_name='Git Clones', index=False)
@@ -136,16 +170,13 @@ def main():
 
     print(f"Data successfully saved to {filename}")
 
-    # Save to MongoDB if connection string is provided
-    if args.mongodb_connection_string:
-        mongo_client = get_mongo_client(args.mongodb_connection_string)
-        database_name = "GitHubTrafficData"
-        save_to_mongodb(mongo_client, database_name, "TrafficStats", traffic_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "GitClones", clones_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "ReferringSites", referrers_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "PopularContent", popular_content_df.to_dict('records'))
+    save_to_mongodb(mongo_client, database_name, "TrafficStats", traffic_df.to_dict('records'))
+    save_to_mongodb(mongo_client, database_name, "GitClones", clones_df.to_dict('records'))
+    save_to_mongodb(mongo_client, database_name, "ReferringSites", referrers_df.to_dict('records'))
+    save_to_mongodb(mongo_client, database_name, "PopularContent", popular_content_df.to_dict('records'))
 
-        print("Data also saved to MongoDB")
+    print("Data also saved to MongoDB")
 
+# Call to main function
 if __name__ == "__main__":
     main()
