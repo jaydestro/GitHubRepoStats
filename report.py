924bc18bcd59fed631c339751021a3ca590f6c26
Jay Gordon <2974195+jaydestro@users.noreply.github.com>
2023-12-19 15:52:43 -0500

    made some changes, just checking this in.  not working properly

--- report.py
+++ report.py
@@ -1,12 +1,12 @@
 import argparse
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date, timezone
 from pymongo import MongoClient
 from azure.cosmos import CosmosClient, PartitionKey
 import json
 from jsonschema import validate, ValidationError
 import requests
-from datetime import datetime
+from pandas._libs.tslibs.timestamps import Timestamp
 
 # Schema for traffic data
 traffic_data_schema = {
@@ -78,7 +78,13 @@ popular_content_data_schema = {
     },
 }
 
-# Function to validate JSON data against a schema
+def convert_to_serializable(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    elif isinstance(obj, Timestamp):
+        return obj.to_pydatetime().isoformat()
+    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
 def validate_json_data(data, schema):
     try:
         validate(instance=data, schema=schema)
@@ -87,7 +93,6 @@ def validate_json_data(data, schema):
         print(f"Validation error: {e.message}")
         return False
 
-# Function to fetch data from GitHub API
 def get_github_data(api_url, token):
     headers = {'Authorization': f'token {token}'}
     response = requests.get(api_url, headers=headers)
@@ -97,82 +102,77 @@ def get_github_data(api_url, token):
         print(f"Failed to fetch data: {response.status_code} - {response.text}")
         return None
 
-# Function to process traffic data (adjusted for JSON serialization)
+# Function to format datetime for serialization
+def format_datetime(timestamp, with_timezone=False):
+    if isinstance(timestamp, datetime):
+        timestamp = timestamp.strftime('%Y-%m-%d')
+    if with_timezone:
+        return datetime.strptime(timestamp, '%Y-%m-%d', tzinfo=timezone.utc).date().isoformat()
+    else:
+        return datetime.strptime(timestamp, '%Y-%m-%d').date().isoformat()
+
+# Function to process traffic data
 def process_traffic_data(data):
     return [{
-        "Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d').date().isoformat(),
-        "Views": item['count'], 
+        "id": item['timestamp'],  # Assuming timestamp can serve as a unique ID
+        "Date": format_datetime(item['timestamp'][:10]),
+        "Views": item['count'],
         "Unique visitors": item['uniques']
     } for item in data['views']]
 
-# Function to process clones data (adjusted for JSON serialization)
+# Function to process clones data
 def process_clones_data(data):
     return [{
-        "Date": datetime.strptime(item['timestamp'][:10], '%Y-%m-%d').date().isoformat(),
-        "Clones": item['count'], 
+        "id": item['timestamp'],
+        "Date": format_datetime(item['timestamp'][:10]),
+        "Clones": item['count'],
         "Unique cloners": item['uniques']
     } for item in data['clones']]
 
-# Updated function to process referrers data with timestamp (adjusted for JSON serialization)
+# Function to process referrers data
 def process_referrers_data(data):
-    timestamp = datetime.now().isoformat()
+    timestamp = format_datetime(datetime.now())
     return [{
-        "Referring site": item['referrer'], 
-        "Views": item['count'], 
+        "id": item['referrer'] + timestamp,  # Create a composite ID
+        "Referring site": item['referrer'],
+        "Views": item['count'],
         "Unique visitors": item['uniques'],
         "FetchedAt": timestamp
     } for item in data]
 
-# Updated function to process popular content data with timestamp (adjusted for JSON serialization)
+# Function to process popular content data
 def process_popular_content_data(data):
-    timestamp = datetime.now().isoformat()
+    timestamp = format_datetime(datetime.now())
     return [{
-        "Path": item['path'], 
-        "Views": item['count'], 
+        "id": item['path'] + timestamp,  # Create a composite ID
+        "Path": item['path'],
+        "Views": item['count'],
         "Unique visitors": item['uniques'],
         "FetchedAt": timestamp
     } for item in data]
 
-# Function to read the Personal Access Token from a file
-def read_token_from_file(file_path):
+# MongoDB utility functions
+def get_mongo_client(connection_string):
     try:
-        with open(file_path, 'r') as file:
-            return file.read().strip()
-    except IOError as e:
-        print(f"Error reading token file: {e}")
+        client = MongoClient(connection_string)
+        return client
+    except Exception as e:
+        print(f"Error creating MongoDB client: {e}")
         return None
 
-# Function to get the last recorded date from a DataFrame
-def get_last_recorded_date(df):
-    if df.empty or "Date" not in df.columns:
-        return None
-    return pd.to_datetime(df["Date"]).max()
+def upsert_to_mongodb(client, database_name, collection_name, data):
+    db = client[database_name]
+    collection = db[collection_name]
+    for item in data:
+        collection.update_one({'id': item['id']}, {'$set': item}, upsert=True)
 
-# Function to append new data to the DataFrame
-def append_new_data(old_df, new_data, date_column):
-    new_df = pd.DataFrame(new_data)
-    if not old_df.empty:
-        last_date = get_last_recorded_date(old_df)
-        new_df = new_df[pd.to_datetime(new_df[date_column]) > last_date]
-    return pd.concat([old_df, new_df], ignore_index=True)
+def fetch_all_from_mongodb(client, database_name, collection_name, date_column):
+    db = client[database_name]
+    collection = db[collection_name]
+    data = list(collection.find())
+    df = pd.DataFrame(data)
+    return process_dataframe(df, date_column)
 
-def add_grouped_totals(df, date_column, metrics_columns):
-    if not df.empty:
-        # Convert the date_column to datetime type
-        df[date_column] = pd.to_datetime(df[date_column])
-
-        # Prepare a DataFrame with only the necessary columns
-        metrics_df = df[[date_column] + metrics_columns]
-
-        # Perform grouping operations
-        monthly_totals = metrics_df.groupby(pd.Grouper(key=date_column, freq="M"))[metrics_columns].sum().reset_index()
-        monthly_totals[date_column] = monthly_totals[date_column].dt.strftime('%Y-%m')
-        yearly_totals = metrics_df.groupby(pd.Grouper(key=date_column, freq="Y"))[metrics_columns].sum().reset_index()
-        yearly_totals[date_column] = yearly_totals[date_column].dt.strftime('%Y')
-
-        # Concatenate the original dataframe with the grouped totals
-        return pd.concat([df, monthly_totals, yearly_totals], ignore_index=True)
-    return df
 
 # Function to create a Cosmos DB client
 def get_cosmos_client(connection_string):
@@ -183,37 +183,58 @@ def get_cosmos_client(connection_string):
         print(f"Error creating Cosmos DB client: {e}")
         return None
 
-# Function to upsert data to Cosmos DB
-def save_to_cosmos_db(client, database_name, collection_name, data):
+def upsert_to_cosmos_db(client, database_name, collection_name, data):
     try:
-        # Create or get database
         database = client.create_database_if_not_exists(id=database_name)
-        # Create or get container
         container = database.create_container_if_not_exists(
             id=collection_name,
             partition_key=PartitionKey(path="/id")
         )
-        # Upsert each item in the data
         for item in data:
-            container.upsert_item(item)
+            item_dict = json.loads(json.dumps(item, default=convert_to_serializable))
+            container.upsert_item(item_dict)
     except Exception as e:
-        print(f"Error saving data to Cosmos DB: {e}")
+        print(f"Error upserting data to Cosmos DB: {e}")
 
-def get_mongo_client(connection_string):
+def fetch_all_from_cosmos_db(client, database_name, container_name, date_column):
     try:
-        client = MongoClient(connection_string)
-        return client
+        database = client.get_database_client(database=database_name)
+        container = database.get_container_client(container=container_name)
+        query = "SELECT * FROM c"
+        items = list(container.query_items(query=query, enable_cross_partition_query=True))
+        
+        # Convert to DataFrame
+        df = pd.DataFrame(items)
+
+        # Process DataFrame to remove duplicates and drop 'id' column
+        df = process_dataframe(df, date_column)
+
+        return df
     except Exception as e:
-        print(f"Error creating MongoDB client: {e}")
+        print(f"Error fetching data from Cosmos DB: {e}")
+        return pd.DataFrame()
+
+def process_dataframe(df, date_column):
+    df = df.drop_duplicates(subset=[date_column])
+    if 'id' in df.columns:
+        df = df.drop(columns=['id'])
+    return df
+
+# Function to save multiple DataFrames to an Excel file
+def save_dataframes_to_excel(filename, **dataframes):
+    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+        for sheet_name, df in dataframes.items():
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+# Function to get GitHub Personal Access Token from a file
+def read_token_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read().strip()
+    except IOError as e:
+        print(f"Error reading token file: {e}")
         return None
-    
-def save_to_mongodb(client, database_name, collection_name, data):
-    try:
-        db = client[database_name]
-        collection = db[collection_name]
-        collection.insert_many(data)
-    except Exception as e:
-        print(f"Error saving data to MongoDB: {e}")
+
 
 # Main function with command-line interface
 def main():
@@ -234,59 +255,75 @@ def main():
     database_name = args.repo.replace("-", "").capitalize()
     filename = args.filename or f"{args.owner}-{args.repo}-traffic-data.xlsx"
 
-    # Fetch and process data
+    # Fetch and process data from GitHub API
     views_data = get_github_data(f"{base_url}/traffic/views", token)
     clones_data = get_github_data(f"{base_url}/traffic/clones", token)
     referrers_data = get_github_data(f"{base_url}/traffic/popular/referrers", token)
     popular_content_data = get_github_data(f"{base_url}/traffic/popular/paths", token)
 
-    # Validate fetched data against schemas
+    # Validate and process data
     if not validate_json_data(views_data, traffic_data_schema) or \
        not validate_json_data(clones_data, clones_data_schema) or \
        not validate_json_data(referrers_data, referrers_data_schema) or \
        not validate_json_data(popular_content_data, popular_content_data_schema):
         return
 
-    traffic_df = append_new_data(pd.DataFrame(), process_traffic_data(views_data), 'Date')
-    clones_df = append_new_data(pd.DataFrame(), process_clones_data(clones_data), 'Date')
-    referrers_df = append_new_data(pd.DataFrame(), process_referrers_data(referrers_data), 'Referring site')
-    popular_content_df = append_new_data(pd.DataFrame(), process_popular_content_data(popular_content_data), 'Path')
+    processed_traffic_data = process_traffic_data(views_data)
+    processed_clones_data = process_clones_data(clones_data)
+    processed_referrers_data = process_referrers_data(referrers_data)
+    processed_popular_content_data = process_popular_content_data(popular_content_data)
 
-    # Add grouped totals for Traffic Stats and Git Clones
-    traffic_df = add_grouped_totals(traffic_df, 'Date', ['Views', 'Unique visitors'])
-    clones_df = add_grouped_totals(clones_df, 'Date', ['Clones', 'Unique cloners'])
-    # Add grouped totals for Referring Sites and Popular Content with timestamp handling
-    referrers_df = add_grouped_totals(referrers_df, 'FetchedAt', ['Views', 'Unique visitors'])
-    popular_content_df = add_grouped_totals(popular_content_df, 'FetchedAt', ['Views', 'Unique visitors'])
-
-    # Save to Excel
-    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
-        traffic_df.to_excel(writer, sheet_name='Traffic Stats', index=False)
-        clones_df.to_excel(writer, sheet_name='Git Clones', index=False)
-        referrers_df.to_excel(writer, sheet_name='Referring Sites', index=False)
-        popular_content_df.to_excel(writer, sheet_name='Popular Content', index=False)
-
-    print(f"Data successfully saved to {filename}")
-
-    # Save to MongoDB if connection string is provided
+    # Check and Initialize MongoDB Client if connection string is provided
+    mongo_client = None
     if args.mongodb_connection_string:
         mongo_client = get_mongo_client(args.mongodb_connection_string)
-        save_to_mongodb(mongo_client, database_name, "TrafficStats", traffic_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "GitClones", clones_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "ReferringSites", referrers_df.to_dict('records'))
-        save_to_mongodb(mongo_client, database_name, "PopularContent", popular_content_df.to_dict('records'))
 
-        print("Data also saved to MongoDB")
-
-    # Save to Cosmos DB if connection string is provided
+    # Check and Initialize Cosmos DB Client if connection string is provided
+    cosmos_client = None
     if args.cosmos_db_connection_string:
         cosmos_client = get_cosmos_client(args.cosmos_db_connection_string)
-        save_to_cosmos_db(cosmos_client, database_name, "TrafficStats", traffic_df.to_dict('records'))
-        save_to_cosmos_db(cosmos_client, database_name, "GitClones", clones_df.to_dict('records'))
-        save_to_cosmos_db(cosmos_client, database_name, "ReferringSites", referrers_df.to_dict('records'))
-        save_to_cosmos_db(cosmos_client, database_name, "PopularContent", popular_content_df.to_dict('records'))
 
-        print("Data also saved to Cosmos DB")
+    # Upsert data into the specified databases
+    if mongo_client:
+        upsert_to_mongodb(mongo_client, database_name, "TrafficStats", processed_traffic_data)
+        upsert_to_mongodb(mongo_client, database_name, "GitClones", processed_clones_data)
+        upsert_to_mongodb(mongo_client, database_name, "ReferringSites", processed_referrers_data)
+        upsert_to_mongodb(mongo_client, database_name, "PopularContent", processed_popular_content_data)
+
+    if cosmos_client:
+        upsert_to_cosmos_db(cosmos_client, database_name, "TrafficStats", processed_traffic_data)
+        upsert_to_cosmos_db(cosmos_client, database_name, "GitClones", processed_clones_data)
+        upsert_to_cosmos_db(cosmos_client, database_name, "ReferringSites", processed_referrers_data)
+        upsert_to_cosmos_db(cosmos_client, database_name, "PopularContent", processed_popular_content_data)
+
+    # Fetch all data from the specified databases and overwrite Excel sheet
+    dataframes_to_save = {}
+    if mongo_client:
+        traffic_df = fetch_all_from_mongodb(mongo_client, database_name, "TrafficStats", "Date")
+        clones_df = fetch_all_from_mongodb(mongo_client, database_name, "GitClones", "Date")
+        referrers_df = fetch_all_from_mongodb(mongo_client, database_name, "ReferringSites", "FetchedAt")
+        popular_content_df = fetch_all_from_mongodb(mongo_client, database_name, "PopularContent", "FetchedAt")
+        dataframes_to_save = {
+            'Traffic Stats': traffic_df,
+            'Git Clones': clones_df,
+            'Referring Sites': referrers_df,
+            'Popular Content': popular_content_df
+        }
+    elif cosmos_client:
+        traffic_df = fetch_all_from_cosmos_db(cosmos_client, database_name, "TrafficStats", "Date")
+        clones_df = fetch_all_from_cosmos_db(cosmos_client, database_name, "GitClones", "Date")
+        referrers_df = fetch_all_from_cosmos_db(cosmos_client, database_name, "ReferringSites", "FetchedAt")
+        popular_content_df = fetch_all_from_cosmos_db(cosmos_client, database_name, "PopularContent", "FetchedAt")
+        dataframes_to_save = {
+            'Traffic Stats': traffic_df,
+            'Git Clones': clones_df,
+            'Referring Sites': referrers_df,
+            'Popular Content': popular_content_df
+        }
+
+    if dataframes_to_save:
+        save_dataframes_to_excel(filename, **dataframes_to_save)
+        print(f"Data successfully saved to {filename}")
 
 if __name__ == "__main__":
     main()
