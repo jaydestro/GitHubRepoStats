0a2fe36c91b6b854a451889be8ba92711f77e9d0
Jay Gordon <2974195+jaydestro@users.noreply.github.com>
2024-04-23 13:54:02 -0400

    improving function

--- function_app.py
+++ function_app.py
@@ -1,5 +1,4 @@
 import datetime
-import subprocess
 import logging
 import os
 import azure.functions as func
@@ -15,17 +14,37 @@ def main(myTimer: func.TimerRequest) -> None:
 
     if myTimer.past_due:
         logging.info('The timer is past due!')
-# retrieve file and parse out owner/repos to collect stats
-# loop here
-    
+
+    # Environment variables for database and storage connections
     mongodb_connection_string = os.environ["CUSTOMCONNSTR_MONGODB"]
     azure_storage_connection_string = os.environ["CUSTOMCONNSTR_BLOBSTORAGE"]
-    
     token = os.environ["GHPAT"]
     output_format = os.environ["OUTPUT"]
 
-    repos = read_file_from_azure_blob(azure_storage_connection_string, "githubrepostats", "repos.csv")
-    for owner, repo in repos:
-        filename = None
-        print("Starting to process {owner}/{repo}")
-        retrieve_and_process_stats(owner, repo, filename, mongodb_connection_string, azure_storage_connection_string, output_format, token)
+    # Retrieve file and parse out owner/repos to collect stats
+    try:
+        repos = read_file_from_azure_blob(azure_storage_connection_string, "githubrepostats", "repos.csv")
+        if repos:
+            total_repos = len(repos)
+            processed_count = 0
+
+            for owner, repo in repos:
+                try:
+                    filename = f"{owner}-{repo}-traffic-data"
+                    logging.info(f"Starting to process {owner}/{repo}")
+                    retrieve_and_process_stats(owner, repo, filename, mongodb_connection_string, azure_storage_connection_string, output_format, token)
+                    processed_count += 1
+                    logging.info(f"Successfully processed {owner}/{repo}")
+                except Exception as e:
+                    logging.error(f"Error processing {owner}/{repo}: {str(e)}")
+
+            if processed_count == total_repos:
+                logging.info("Successfully processed all repositories.")
+            else:
+                logging.warning(f"Processed {processed_count} out of {total_repos} repositories. Some repositories failed to process.")
+
+        else:
+            logging.warning("No repositories found to process.")
+    except Exception as e:
+        logging.error(f"Failed to read from Azure Blob or process data: {str(e)}")
+
